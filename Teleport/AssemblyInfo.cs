16330bc1a39327e2a9a11f1441a2f1de5428292f
Li Fangyu <ccccces@163.com>
2025-11-04 17:46:33 +0800

    移除Teleport目录

--- Teleport/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("MapTeleport")]
-[assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyProduct("MapTeleport")]
-[assembly: AssemblyTitle("MapTeleport")]
-[assembly: AssemblyVersion("1.0.0.0")]
